2837b33c990fa9ca35359ea35c248a8a9eaa5680
shalun002 <32149071+shalun002@users.noreply.github.com>
2018-05-02 18:02:32 +0600

    Add project files.

---
+++ Array/Module/Index.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array.Module
+{
+    public class Index
+    {
+        private int[] _array;
+
+        public Index()
+        {
+            Size = 1;
+            _array = new int[Size];
+        }
+
+        public Index(int size)
+        {
+            Size = size;
+            _array = new int[Size];
+        }
+
+        public int Size { get; set; }
+
+        public int this[int i]
+        {
+            get
+            {
+                return _array[i];
+            }
+            set
+            {
+                _array[i] = (int)Math.Pow((double)value, 2);
+            }
+        }
+    }
+}
---
+++ Module_8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Array.Module;
+
+
+/// <summary>
+/// 1.	Создать индексатор для одномерного массива который при установке значения будет возводить в квадрат передаваемое значение переменной
+/// и устанавливать его для указанного индекса. При получении элемента массива по индексу будет возвращаться его текущее значение.
+/// </summary>
+
+
+namespace Module_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Index index = new Index(3);
+            index[0] = 4;
+            index[1] = 8;
+            index[2] = 10;
+
+            for (int i = 0; i < index.Size; i++)
+            {
+                Console.Write(index[i] + "\t");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
